558adeb1fe3f1d345a8fb8972647305976994cd8
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2023-01-19 17:26:59 +0900

    123

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import React from "react";
 import AppRouter from "components/Router";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import {authService} from "fbase";
 function App() {
-  const [isLoggedIn,setIsLoggedIn]=useState(authService.currentUser)
-  console.log(authService.currentUser)
-
+  const auth = getAuth();
+  const [init,setInit]=useState(false) //로딩창으로 대체하면 될듯
+  const [isLoggedIn,setIsLoggedIn]=useState(false)
+  useEffect(()=>{
+    onAuthStateChanged(auth,(user)=>{ //user의 값이 바뀔 때마다 실행됨
+      console.log(auth)
+      if(user){ //이미 로그인 했을때
+        setIsLoggedIn(true)
+      } else{
+        setIsLoggedIn(false)
+      }
+      setInit(true)
+    })
+  },[])
   return (
-    <div><AppRouter isLoggedIn={isLoggedIn}/></div>
+    <>
+      {init ? <AppRouter isLoggedIn={isLoggedIn}/> : "Initailizing"}
+    </>
   );
 }
 
 export default App;
+
+//isLoggedIn이 true라는 것은 현재 사용자가 로그인 했다는 뜻 이므로
+//approuter 컴포넌트에 isloggedin 프롭에 true값 전달되고
+//홈 화면 보여줌
+
+//onAuthStateChanged
+//user가 트루라는것은 이미 로그인햇다는뜻
+
+//새로고침시
+//init이 false이므로 아주 잠깐동안 initializing나옴
+//firebase불러오면 user 값에 따라 isloggedin 값 바뀌고
+//init 값 true로 바뀌고 home컴포넌트 불러옴
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,10 +1,11 @@
-import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth,signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword,signInWithEmailAndPassword, GithubAuthProvider } from "firebase/auth";
 import React, { useState } from "react";
 
 const Auth= ()=>{
     const [email,setEmail]=useState("")
     const [password,setPassword]=useState("")
     const [newAccount,setNewAccount]=useState(true) //true일때는 create acc, false 일때는 login
+    const [error,setError]=useState("")
     const onChange=(event)=>{
         const {target:{name,value}}=event
         if(name==="email"){
@@ -21,17 +22,28 @@ const Auth= ()=>{
             let data
             if(newAccount){ //create acc
                 data = await createUserWithEmailAndPassword(auth,email,password)
-                
+                setNewAccount(false)
+
             }
             else{ //log in
                 data = await signInWithEmailAndPassword(auth,email,password)
             }
         }
         catch(error){
-            const erroCode=error.code
-            const errorMsg=error.message
-            alert(erroCode)
+            setError(error.message)
         }
+    }
+    const toggleAccount=()=>setNewAccount((prev)=>!prev)
+    const onSocialClick=async(event)=>{
+        const auth=getAuth()
+        const {target:{name}}=event
+        let provider
+        if(name==="google"){
+            provider=new GoogleAuthProvider()
+        }else if(name==="github"){
+            provider=new GithubAuthProvider()
+        }
+        const data=await signInWithPopup(auth,provider)
     }
     return (
         <div>
@@ -51,10 +63,12 @@ const Auth= ()=>{
                     value={password}
                     onChange={onChange}/>
                 <input type="submit" value={newAccount ? "Create Account" : "Log In"}/>
+                {error}
             </form>
+            <span onClick={toggleAccount}>{newAccount ? "Sign in" : "Create Account"}</span>
             <div>
-                <button>Continue with Google</button>
-                <button>Continue with Github</button>
+                <button onClick={onSocialClick} name="google">Continue with Google</button>
+                <button onClick={onSocialClick} name="github">Continue with Github</button>
             </div>
         </div>
     )
@@ -62,3 +76,9 @@ const Auth= ()=>{
 export default Auth
 
 // https://firebase.google.com/docs/auth/web/password-auth?authuser=1#web-version-9
+
+//Auth 컴포넌트에서 이메일을 만들고 로그인하던,
+//구글로 로그인하던,깃허브로 로그인하던
+//로그인 성공하면 user정보가 채워지고
+//user값이 변화하는지 지켜보는 app.js컴포넌트의 onAuthStateChanged함수가
+//isloggenin값 변화에 따라 다음 컴포넌트 보여줌
