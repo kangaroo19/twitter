fe6b3e0b5cf68a2d2ff6bd9f493d3bf46b8f546e
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2023-01-22 21:07:41 +0900

    2023/01/22

---
+++ src/components/Nweet.js
@@ -0,0 +1,55 @@
+import React from "react";
+import {doc,deleteDoc, updateDoc} from 'firebase/firestore'
+import { dbService } from "fbase";
+import { useState } from "react";
+const Nweet=({nweetObj,isOwner})=>{
+    const [editing,setEditing]=useState(false) //edit form 토글버튼 위함 true면 edit form 나옴
+    const [newNweet,setNewNweet]=useState(nweetObj.text) //nweet의 값 수정가능
+    const onDeleteClick=async()=>{
+        const ok=window.confirm("Are you sure")
+        if(ok){
+            await deleteDoc(doc(dbService,"nweets",`${nweetObj.id}`)) //path는 아이디값 (사용자 아이디값 아님)
+        }
+    }
+    const toggleEditing=()=>setEditing((prev)=>!prev) 
+    const onSubmit=async(event)=>{
+        event.preventDefault()
+        await updateDoc(doc(dbService,"nweets",`${nweetObj.id}`),{text:newNweet})
+        setEditing(false)
+    }
+    const onChange=(event)=>{
+        const {target:{value}}=event
+        setNewNweet(value)
+    }
+    return (
+        <div>
+           {
+                editing ? (
+                    <>
+                        <form onSubmit={onSubmit}>
+                            <input 
+                                type="text" 
+                                placeholder="Edit your nweet"
+                                value={newNweet} 
+                                required
+                                onChange={onChange}/>
+                            <input type="submit" value="Update Nweet"/>
+                        </form>
+                        <button onClick={toggleEditing}>Cancel</button>
+                    </>
+                ) : (
+                    <>
+                        <h4>{nweetObj.text}</h4>
+                        {isOwner && ( 
+                            <>
+                            <button onClick={onDeleteClick}>Delete Nweet</button>
+                            <button onClick={toggleEditing}>Edit Nweet</button>
+                            </>
+                        )}
+                    </>
+            )}
+        </div>
+    )
+}
+
+export default Nweet
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,16 +2,15 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 import { collection, addDoc,serverTimestamp,getDocs,query,onSnapshot,doc, QuerySnapshot, orderBy  } from "firebase/firestore";
 import { dbService } from "fbase";
-export default ({userObj})=>{
+import Nweet from "components/Nweet";
+export default ({userObj})=>{ //userObj는 유저의 고유한 식별번호
     const [nweet,setNweet]=useState("")
     const [nweets,setNweets]=useState([])
-    
-   
+    const [attachment,setAttachment]=useState()
     useEffect(()=>{
         const q=query(collection(dbService,"nweets"),orderBy("date","desc"))
-        const un=onSnapshot(q,(snapshot)=>{
+        const un=onSnapshot(q,(snapshot)=>{ //데이터베이스에 변화가 생기면 onSnapshot 실행됨
             const arr=[]
-            console.log(123)
             snapshot.forEach((doc)=>{
                 arr.push({
                     id:doc.id, //여기 id는 creatorid와 다름
@@ -28,7 +27,8 @@ export default ({userObj})=>{
               text : nweet,
               date : serverTimestamp(), //이거때문에 두번호출됨
               creatorId : userObj.uid
-            });
+            }); //위의 객체(nweet)가 Nweet컴포넌트의 nweetObj프롭으로 들어감
+
           } catch (e) {
             console.error("Error adding document: ", e);
           }
@@ -38,17 +38,36 @@ export default ({userObj})=>{
         const {target:{value}}=event
         setNweet(value)
     }
+    const onFileChange=(event)=>{
+        const {target:{files}}=event
+        const theFile=files[0]
+        const reader=new FileReader()
+        reader.onloadend=(finishedEvent)=>{
+            const {currentTarget:{result}}=finishedEvent
+            setAttachment(result)
+        }
+        reader.readAsDataURL(theFile)
+    }
+    const onClearAttachment=()=>setAttachment(null)
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input value={nweet} onChange={onChange}type="text" placeholder="what s on your mind" maxLength={120}/>
+                <input type="file" onChange={onFileChange} accept="image/*"/>
                 <input type="submit" value="tweet"/>
+                {attachment && 
+                    <div>
+                        <img src={attachment} alt="" width="50px" height="50px"/>
+                        <button onClick={onClearAttachment}>Clear</button>
+                    </div>}
+                
             </form>
             <div>
                 {nweets.map((nweet) => (
-                <div key={nweet.id}>
-                    <h4>{nweet.text}</h4>
-                </div>
+                    <Nweet 
+                        key={nweet.id} 
+                        nweetObj={nweet} 
+                        isOwner={nweet.creatorId===userObj.uid}/> //nweet를 작성한 아이디값(nweet.creatorid)과 현재 로그인한 사용자의 아이디값(userObj.uid)이 같으면 true
                 ))}
             </div>
         </div>
